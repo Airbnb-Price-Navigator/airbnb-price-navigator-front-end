e03fefaa3f855a458770306fa87c6147e47811bd
Kevin Smith <keveightysev@gmail.com>
2019-09-10 15:23:13 -0500

    Fixed css reset, imported font

--- src/App.js
+++ src/App.js
@@ -4,9 +4,12 @@ import Nav from './components/Nav';
 import Main from './components/Main';
 import Footer from './components/Footer';
 
+import GlobalStyles from './styles/Global';
+
 const App = () => {
 	return (
 		<>
+			<GlobalStyles />
 			<Nav />
 			<Main />
 			<Footer />
