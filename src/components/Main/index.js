471aee40d0ee97aa3d62f84a8053e33273d3dad5
Tomas Alexander Fox <TomasFox@Tomass-MacBook-Air.local>
2020-07-22 18:40:03 -0300

    updating ds API w/ no cloudfront

--- src/components/Main/index.js
+++ src/components/Main/index.js
@@ -47,7 +47,7 @@ const Main = () => {
 			zipcode: form.zipcode.toString(),
 		});
 		const results = await axios.post(
-			'https://dnhbfbsykiic0.cloudfront.net/prediction',
+			'http://aitbnbnew.eba-pk9ts2rm.us-east-2.elasticbeanstalk.com/prediction',
 			form,
 		);
 		const dataCheck = results.data.plot_values.reduce((acc, curr) => {
